43c3ad7ea7323d043515c3443e511cd0b0652afe
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-01-03 13:29:58 +0300

    Added temperature in Celcium degrees

--- axios-task/app.js
+++ axios-task/app.js
@@ -1,9 +1,14 @@
 axios
 	.get(
-		'https://api.openweathermap.org/data/2.5/weather?q=Minsk&APPID=3eccf5edf65b61a0d3a4e1be2516e2fc'
+		'https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&APPID=3eccf5edf65b61a0d3a4e1be2516e2fc'
 	)
 	.then(function(response) {
-		console.log(response);
+		let tempInCelcium = response.data.main.temp;
+		console.log(
+			`Temp in Minsk in Celsiun ${tempInCelcium.toFixed(2)} degrees.`
+		);
+		let data = response.data;
+		console.log(data);
 	})
 	.catch(function(error) {
 		console.log(error);
