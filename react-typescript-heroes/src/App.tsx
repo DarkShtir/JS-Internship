07c2495d48646af7f1e145575b642db754f1af27
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-01-27 21:01:17 +0300

    First version simple ToDo List on React-TypeScript

--- react-typescript-heroes/src/App.tsx
+++ react-typescript-heroes/src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Navbar } from './components/Navbar';
+import { ToDoForm } from './components/ToDoForm';
+import { TodoList } from './components/TodoList';
+import { ITodo } from './interfaces';
+
+declare var confirm: (question: string) => boolean;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [todos, setTodos] = useState<ITodo[]>([]);
+
+	useEffect(() => {
+		const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[];
+		setTodos(saved);
+	}, []);
+
+	useEffect(()=>{
+		localStorage.setItem('todos', JSON.stringify(todos))
+	},[todos])
+
+	const addHandler = (title: string) => {
+		const newTodo: ITodo = {
+			title: title,
+			id: Date.now(),
+			completed: false,
+		};
+		// setTodos([newTodo, ...todos]);
+		setTodos(prev => [newTodo, ...prev]);
+	};
+
+	const toggleHandler = (id: number) => {
+		setTodos(prev =>
+			prev.map(todo => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		);
+	};
+
+	const removeHandler = (id: number) => {
+		const shouldRemove = confirm(
+			'Вы уверены что хотите удалить элемент???'
+		);
+		if (shouldRemove) {
+			setTodos(prev => prev.filter(todo => todo.id !== id));
+		}
+	};
+
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<ToDoForm onAdd={addHandler} />
+
+				<TodoList
+					todos={todos}
+					onToggle={toggleHandler}
+					onRemove={removeHandler}
+				/>
+			</div>
+		</>
+	);
+};
 
 export default App;
