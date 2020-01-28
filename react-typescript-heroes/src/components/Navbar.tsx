c10c7adc3f485d07a98f4670cb75598a39bf6fac
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-01-28 13:27:15 +0300

    Updated and fixed NavBar

--- react-typescript-heroes/src/components/Navbar.tsx
+++ react-typescript-heroes/src/components/Navbar.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export const Navbar: React.FC = () => {
-  return (
-    <>
-        <nav>
-      <div className="nav-wrapper deep-purple darken-3" >
-        <a href="/" className="brand-logo center">React+TypeScript</a>
-        <a href="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-        <ul className="left hide-on-med-and-down">
-          <li><a href="/">Список дел</a></li>
-          <li><a href="/">Информация</a></li>
-        </ul>
-      </div>
-    </nav>
-    
-    <ul className="sidenav">
-      <li><a href="/">Список дел</a></li>
-      <li><a href="/">Информация</a></li>
-    </ul>
-    </>
-  )
-}
+	return (
+		<>
+			<nav>
+				<div className="nav-wrapper deep-purple darken-3">
+					<a href="/" className="brand-logo center">
+						React+TypeScript
+					</a>
+
+					<ul className="right">
+						<li>
+							<a href="/">Список дел</a>
+						</li>
+						<li>
+							<a href="/">Информация</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+};
